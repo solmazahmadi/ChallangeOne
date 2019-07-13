f9862c117733afe8284f662044c1a7642fb26e80
solmazahmadi <solmaz.ahmadi@ut.ac.ir>
2019-07-13 11:40:23 +0430

    #ssss

--- for instance.py
+++ for instance.py
@@ -1,13 +1,13 @@
 file_name =['D:\\Python./files\\1016341429.txt', 'D:\\Python./files\\1016396850.txt',
       'D:\\Python./files\\1020216517.txt', 'D:\\Python./files\\1021190361.txt',
       'D:\\Python./files\\1022093567.txt',
-      'D:\\Python./files\\1025312293.txt',]
+      'D:\\Python./files\\1025312293.txt', 'D:\\Python./files\\2501296572.txt']
 print(len(file_name))
 
 text_inside =['1016341429', '1016396850', '1025312293']
 
-for element in file_name:
-    for ele in text_inside:
+for ele in text_inside:
+    for element in file_name:
         if ele in element:
             file_name.remove(element)
 print(file_name)
