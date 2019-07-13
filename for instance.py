6c29620efc6095fe87385750c7a94ec0a2deec22
solmazahmadi <solmaz.ahmadi@ut.ac.ir>
2019-07-13 10:58:16 +0430

    # a simple example for Q.2

---
+++ for instance.py
@@ -0,0 +1,16 @@
+file_name =['D:\\Python./files\\1016341429.txt', 'D:\\Python./files\\1016396850.txt',
+      'D:\\Python./files\\1020216517.txt', 'D:\\Python./files\\1021190361.txt',
+      'D:\\Python./files\\1022093567.txt',
+      'D:\\Python./files\\1025312293.txt',]
+print(len(file_name))
+
+text_inside =['1016341429', '1016396850', '1025312293']
+
+for element in file_name:
+    for ele in text_inside:
+        if ele in element:
+            file_name.remove(element)
+print(file_name)
+print(len(file_name))
+
+
